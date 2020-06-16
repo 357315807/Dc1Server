332e5f61d0e5ddf49daf7000f88367375cdf441c
357315807@qq.com <li13227226592>
2020-06-16 11:07:32 +0800

    解决跨域问题，新增CronConfig

---
+++ src/main/java/space/ponyo/dc1/server/config/CronConfig.java
@@ -0,0 +1,16 @@
+package space.ponyo.dc1.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CronConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+}
